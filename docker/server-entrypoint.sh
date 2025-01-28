c1d402aca557d7fb69e78acfcd39806890cb3326
rufat200 <habibrahmanow07@mail.ru>
2025-01-28 14:01:41 +0600

    Added: Docker, Nginx

---
+++ docker/server-entrypoint.sh
@@ -0,0 +1,4 @@
+python manage.py
+python manage.py collectstatic --noinput
+
+exec gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4
--- main/settings.py
+++ main/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3ut3hxjbsf0$i&wjru5ttw96c%l1y1al^9%hk_qf_=0r@-r$-n'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*")]
 
 
 # Application definition
@@ -82,11 +85,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'ENGINE': os.getenv("ENGINE"),
+
+        'NAME': os.getenv("DB_NAME"),
+
+        'USER': os.getenv("DB_USER"),
+
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+
+        'HOST': os.getenv("DB_HOST"),
+
+        'PORT': os.getenv("DB_PORT"),
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -123,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
